8d7be65c75e7e9c01af3c443ddbc291d9ec8e8a5
Anshuman Sharma <anshuman9998@gmail.com>
2024-03-13 14:56:20 +0530

    typescript build

--- ecosystem.config.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-    apps: [
-      {
-        name: 'app.ts',
-        script: 'src/app.ts',
-        interpreter: '/home/ubuntu/.nvm/versions/node/v18.16.0/bin/ts-node', 
-      }
-    ]
-  };
