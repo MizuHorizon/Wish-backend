2325929296c6ba0d2910c46e892a2e63cdeeb58f
Anshuman Sharma <anshuman9998@gmail.com>
2024-03-14 17:37:44 +0530

    fixed a bug

--- src/routes/v1/productRoutes.ts
+++ src/routes/v1/productRoutes.ts
@@ -19,10 +19,11 @@ import {getProductDetails} from "../../middlewares/getProductDetails";
 
 //app.post("/:id", getProductDetails,createProduct);
 app.post("/:userid", processProductRequest);
-app.get("/:id", getProductbyId);
+
 app.get("/", getProductbyIdOfUser);
 app.get("/track", getTrackableProductbyIdOfUser);
 app.patch("/:id", updatePriceOfProduct);
+app.get("/:id", getProductbyId);
 app.patch("/disable/:id",disableProductTracker);
 app.patch("/enable/:id",enableProductTracker);
 app.patch("/price/:id",changeDesiredPrice);
