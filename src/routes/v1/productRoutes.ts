2f5c6e1a4c04db906d6ea0622835ae8621ae27e0
Anshuman Sharma <anshuman9998@gmail.com>
2024-03-14 17:48:43 +0530

    tests

--- src/routes/v1/productRoutes.ts
+++ src/routes/v1/productRoutes.ts
@@ -19,11 +19,10 @@ import {getProductDetails} from "../../middlewares/getProductDetails";
 
 //app.post("/:id", getProductDetails,createProduct);
 app.post("/:userid", processProductRequest);
-
 app.get("/", getProductbyIdOfUser);
 app.get("/track", getTrackableProductbyIdOfUser);
 app.patch("/:id", updatePriceOfProduct);
-app.get("/:id", getProductbyId);
+// app.get("/:id", getProductbyId);
 app.patch("/disable/:id",disableProductTracker);
 app.patch("/enable/:id",enableProductTracker);
 app.patch("/price/:id",changeDesiredPrice);
