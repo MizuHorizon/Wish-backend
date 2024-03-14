1f067ce5fa4dd1b03ba1900c2e7855477c00034a
Anshuman Sharma <anshuman9998@gmail.com>
2024-03-14 17:15:44 +0530

    product route

--- src/routes/v1/productRoutes.ts
+++ src/routes/v1/productRoutes.ts
@@ -18,7 +18,7 @@ import {getProductDetails} from "../../middlewares/getProductDetails";
 
 
 //app.post("/:id", getProductDetails,createProduct);
-app.post("/:id", processProductRequest);
+app.post("/:id/process", processProductRequest);
 app.get("/:id", getProductbyId);
 app.get("/", getProductbyIdOfUser);
 app.get("/track", getTrackableProductbyIdOfUser);
