386e1556f652ae968aba788b23b5db3b76757767
Anshuman Sharma <anshuman9998@gmail.com>
2024-03-28 23:27:39 +0530

    raw stats

--- src/routes/v1/index.ts
+++ src/routes/v1/index.ts
@@ -1,10 +1,24 @@
 import Express from "express";
 const app = Express.Router();
-
+import prisma from "../../prisma/index";
+const { user,product } = prisma;
 import userRoutes from "../v1/userRoutes";
-
 import productRoutes from "../v1/productRoutes";
 
+
+
+app.get("/stats",async(req,res)=>{
+  const u = await user.findMany();
+  const p = await product.findMany();
+      return res.status(200).json({
+         message : "SuccessFully fetched server stats!",
+         success:true,
+         data : {
+           users : u.length,
+           products : p.length,
+         }
+      })
+})
 app.get("/check", (req, res) => {
   res.status(200).json({ message: "Hello  OK From the Server" });
 });
